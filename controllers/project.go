5b7faa882178a98c8c0112f271ea839f2c5a05ee
GS.Won <bluemirr5@gmail.com>
2014-07-28 16:50:05 +0900

    add modify user module

---
+++ controllers/project.go
@@ -0,0 +1,57 @@
+package controllers
+
+import (
+	"bitbucket.org/bluemirr/schedo/models"
+	"encoding/json"
+	"github.com/astaxie/beego"
+)
+
+type ProjectController struct {
+	beego.Controller
+}
+
+// @router / [get]
+func (this *ProjectController) Get() {
+	//TODO
+	bodyMap := make(map[string]interface{})
+	bodyMap["userId"] = "id"
+	this.Data["json"] = models.SuccessResult(bodyMap)
+	this.ServeJson()
+}
+
+// @router / [post]
+func (this *ProjectController) Post() {
+	var project models.ProjectParam
+	parseErr := json.Unmarshal(this.Ctx.Input.CopyBody(), &project)
+	if parseErr != nil {
+		this.Data["json"] = models.NewApiResult(400, parseErr, "bad request")
+		this.ServeJson()
+	}
+
+	err := project.InsertProject()
+	if err != nil {
+		this.Data["json"] = models.NewApiResult(500, err, "not operated")
+		this.ServeJson()
+	}
+
+	bodyMap := make(map[string]interface{})
+	bodyMap["userId"] = "id"
+	this.Data["json"] = models.SuccessResult(bodyMap)
+	this.ServeJson()
+}
+
+// @router / [put]
+func (this *ProjectController) Put() {
+	bodyMap := make(map[string]interface{})
+	bodyMap["userId"] = "id"
+	this.Data["json"] = models.SuccessResult(bodyMap)
+	this.ServeJson()
+}
+
+// @router / [delete]
+func (this *ProjectController) Delete() {
+	bodyMap := make(map[string]interface{})
+	bodyMap["userId"] = "id"
+	this.Data["json"] = models.SuccessResult(bodyMap)
+	this.ServeJson()
+}
